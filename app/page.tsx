86aa1a97333360a3ad7009853f960bb69c61e504
Charles <agbakwucharles@gmail.com>
2025-02-02 18:32:38 -0600

    Update page.tsx
    Removed unused function

--- app/page.tsx
+++ app/page.tsx
@@ -14,28 +14,6 @@ const getUsers = async () => {
   }
 };
 
-const createUser = async () => {
-  const newUser = prompt("Enter new user name");
-  if (!newUser) return;
-
-  try{
-    const res = await fetch('http://localhost:3000/api/users', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ name: newUser })
-    });
-    if (!res.ok) {
-      throw new Error('Failed to fetch data');
-    }
-
-    return res.json();
-  } catch (error) {
-    console.error('Error fetching users:', error);
-  }
-}
-
 export default async function Home() {
   const { users } = await getUsers();
   
